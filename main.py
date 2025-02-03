4aa854fc844856f31f6791b1bef4833562ecb5b3
Goodsie <gidsola@users.noreply.github.com>
2025-02-03 17:00:45 -0500

    linting corrections

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
 from typing import Union
 from fastapi import FastAPI
-import subprocess
 
 app = FastAPI();
 
@@ -8,12 +8,14 @@ app = FastAPI();
 async def image(text: str, url: Union[str, None] = None):
   script_path = "/media/goodsie/codes/Repos/pythin/image/describe/SmolVLM.py"; # need to change this up
   args = [text];
-  if url: args.append(url)
+  if url: 
+    args.append(url)
     
   result = subprocess.run(
     ['python', script_path] + args, 
     capture_output=True, 
     text=True
+    check=True
   );
   
   return {"output":result.stdout};
