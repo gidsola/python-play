74253f8f56530bb0e1d1dedf549186d616e79b16
Goodsie <gidsola@users.noreply.github.com>
2025-02-17 09:14:27 -0500

    Update reqs + Linter

--- endpoints/tts/parlerTTS-Tiny.py
+++ endpoints/tts/parlerTTS-Tiny.py
@@ -3,17 +3,21 @@ from parler_tts import ParlerTTSForConditionalGeneration
 from transformers import AutoTokenizer
 import soundfile as sf
 
-device = "cuda:0" if torch.cuda.is_available() else "cpu"
+DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-model = ParlerTTSForConditionalGeneration.from_pretrained("parler-tts/parler-tts-tiny-v1").to(device)
+model = ParlerTTSForConditionalGeneration.from_pretrained(
+    "parler-tts/parler-tts-tiny-v1"
+    ).to(DEVICE)
+
 tokenizer = AutoTokenizer.from_pretrained("parler-tts/parler-tts-tiny-v1")
 
-prompt = "Hey, how are you doing today?"
-description = "Jon's voice is monotone yet slightly fast in delivery, with a very close recording that almost has no background noise."
+PROMPT = "Hey, how are you doing today?"
+DESCRIPTION = "Monotone yet slightly fast in delivery, with a slight accent."
 
-input_ids = tokenizer(description, return_tensors="pt").input_ids.to(device)
-prompt_input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+input_ids = tokenizer(DESCRIPTION, return_tensors="pt").input_ids.to(DEVICE)
+prompt_input_ids = tokenizer(PROMPT, return_tensors="pt").input_ids.to(DEVICE)
 
 generation = model.generate(input_ids=input_ids, prompt_input_ids=prompt_input_ids)
 audio_arr = generation.cpu().numpy().squeeze()
+
 sf.write("parler_tts_out.wav", audio_arr, model.config.sampling_rate)
